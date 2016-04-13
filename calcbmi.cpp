1d61b5b247c93c964a4c13c1b2d466464341735b
stoslo <sct24@email.vccs.edu>
2016-04-13 17:41:31 -0400

    Update calcbmi.cpp

--- calcbmi.cpp
+++ calcbmi.cpp
@@ -10,65 +10,66 @@ using namespace std;
 int main()
 {
     //Declare storage variables for User input and calculated output integers for the input and float for the output
-    int UserHeight;
-    int UserWeight;
-    int UserAge;
+    int UserHeight; //Height
+    int UserWeight; //Weight
     float BMI;
 
     //Welcome the User to the program and give them brief instructions
     cout << "Greetings, and welcome to calcBMI!" << endl;
     cout << "We will be asking you a series of questions to calculate your Body Mass Index." << endl;
 
-//Begin User input section
-//Height input
+    //Begin User input section
+    //Height input
 do{
     cout << "First, please enter your height in inches. (Example: 72) Then press enter." << endl;
     cin >> UserHeight;
 
-if (!cin){
-            cout << "Not a number! " << endl;
-            cin.clear();
-        }
-        cin.ignore(100, '\n');
-    if ((UserHeight<22)||(UserHeight>107))
-        cout << "You have enter a value out of our range. " << endl;
-}while((UserHeight<22)||(UserHeight>107));
-//Weight input
-do{
-    cout << "Next, please enter your weight in pounds. (Example: 180) Then press enter. ";
-    cin >> UserWeight;
+    //Test if height is an invalid entry and raise an error
     if (!cin){
             cout << "Not a number! " << endl;
             cin.clear();
         }
         cin.ignore(100, '\n');
+    //Test is number entered is outside of our range    
+    if ((UserHeight<22)||(UserHeight>107))
+        cout << "You have entered a value out of range. " << endl;
+}
+//Continue if height is good input
+while((UserHeight<22)||(UserHeight>107));
+//Ask the user for their weight
+do{
+    cout << "Next, please enter your weight in pounds. (Example: 180) Then press enter. ";
+    cin >> UserWeight;
+    //If the value entered is not a number, raise a warning
+    if (!cin){
+            cout << "Not a number! " << endl;
+            cin.clear();
+        }
+        cin.ignore(100, '\n');
+        
+    //Check if User entered value outside of range
     if ((UserWeight>400)||(UserWeight<80))
         cout << "You have enter a value out of our range. " << endl;
-}while((UserWeight>400) ||(UserWeight<80));
-
-    //We need to add some input validation here for UserHeight and UserWeight
-    //Section coming soon
-
+        
+}
+while((UserWeight>400) ||(UserWeight<80));
     //Calculate the BMI here and store it to the float BMI
     BMI = (UserWeight * 703) / (UserHeight * UserHeight);
 
     //Now we need to output the BMI result to the User
     cout << "Your BMI is " << BMI << endl;
 
-    //Section where we inform the User of their body type based on BMI value and Age.
-    //Section coming soon
-
-    //if loop
+    //Output the User's body type based on their calculated BMI
     if (BMI<=18.5)
-        cout << "You are underweight";
+        cout << "Based on your BMI result, your body type is considered below average";
     else if(BMI<=24.9)
-        cout << "You have a normal BMI";
+        cout << "Based on your BMI result, your body type is considered average.";
     else if(BMI<=29.9)
-        cout << "You are overweight"; //the teacher wants somthing "nicer"
-    else if(BMI<=40)                 //than obese and extremely obese 
-        cout << "You are obese";     //Team captain veto. I feel the terms we use are the most politically correct. Maybe we can word our output a little better instead.
+        cout << "Based on your BMI result, your body type is considered above average."; //the teacher wants somthing "nicer"
+    else if(BMI<=40)                                                                    //than obese and extremely obese 
+        cout << "Based on your BMI result, your body type is considered slightly overweight.";
     else if (BMI>40)
-        cout << "Based on your personal information, you are quite obese.";
+        cout << "Based on your BMI result, your body type is considered overweight.";
 
    return 0;
 }
