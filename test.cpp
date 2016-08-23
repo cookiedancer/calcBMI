ee0d46671532d0691f78b77fbeb59faa34ec0aec
stoslo <sct24@email.vccs.edu>
2016-08-22 20:54:34 -0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool TestingOutput = true;
+
+string Statement = "Hello World";
+
+int main()
+{
+    if(TestingOutput == true)
+    {
+        cout << Statement << endl;
+    }
+    return 0;
+}
+
